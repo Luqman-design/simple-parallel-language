d5e495dfc087ca374e7a684a9fb93b50b6eab55b
MunasarMohamed <mmam24@student.aau.dk>
2026-03-26 22:46:39 +0100

    Semantic Analyzer mangler lille update

--- semantic_analyzer.c
+++ semantic_analyzer.c
@@ -54,7 +54,7 @@ steps:
 ]
 
 */
-#define MAX_SCOPE 100
+#define MAX_STATE 100
 
 typedef struct {
   char name[32];
@@ -62,7 +62,7 @@ typedef struct {
   UT_hash_handle hh;
 } VariableEntry;
 
-VariableEntry *states[MAX_SCOPE];
+VariableEntry *states[MAX_STATE];
 int state_top = -1;
 
 void enter_state() {
@@ -89,11 +89,11 @@ void insert_variable(const char *name, TokenType type) {
   HASH_FIND_STR(states[state_top], name, v);
 
   if (v != NULL) {
-    printf("Semantic error: Variable %s is already declared", name);
+    printf("Semantic error: Variable %s is already declared\n", name);
     exit(1);
   }
   v = malloc(sizeof(VariableEntry));
-  strcopy(v->name, name);
+  strcpy(v->name, name);
   v->type = type;
 
   HASH_ADD_STR(states[state_top], name, v);
@@ -111,37 +111,122 @@ VariableEntry *lookup_variable(const char *name) {
   return NULL;
 }
 
-
-/* One struct = one hashmap entry
-typedef struct {
-    char key[32];
-    int  value;
-    UT_hash_handle hh;  // makes this struct hashable
-} Item;
-
-int main(void) {
-    / Dynamic array of hashmap pointers /
-    int n = 2;
-    Item **maps = calloc(n, sizeof(Item));  // [{}, {}]
-
-    * Fill map[0] /
-    Iteme = malloc(sizeof(Item));
-    strcpy(e->key, "score");  e->value = 42;
-    HASH_ADD_STR(maps[0], key, e);
-
-    * Fill map[1] /
-    e = malloc(sizeof(Item));
-    strcpy(e->key, "score");  e->value = 99;
-    HASH_ADD_STR(maps[1], key, e);
-
-    / Read back /
-    for (int i = 0; i < n; i++) {
-        Itemfound;
-  HASH_FIND_STR(maps[i], "score", found);
-        if (found) printf("map[%d] score = %d\n", i, found->value);
+TokenType analyze_expression(Node *node) {
+  switch (node->type) {
+    case NODE_INT_VALUE:
+      return TOKEN_INT_TYPE;
+    case NODE_STRING_VALUE:
+      return TOKEN_STRING_TYPE;
+    case NODE_IDENTIFIER: {
+      VariableEntry *v = lookup_variable(node->body.identifier.name);
+      if (v == NULL) {
+        printf("Semantic error: Variable %s is not declared\n", node->body.identifier.name);
+        exit(1);
+      }
+      return v->type;
     }
-
-    return 0;
+    case NODE_BINARY_OPERATION: {
+      TokenType left_type = analyze_expression(node->body.binary_operation.left_operand);
+      TokenType right_type = analyze_expression(node->body.binary_operation.right_operand);
+      if (left_type != right_type) { // Skal opdateres til at tjekke operator type
+        printf("Semantic error: Type mismatch in binary operation\n");
+        exit(1);
+      }
+      return left_type; 
+    }
+    case NODE_UNARY_OPERATION: {
+      TokenType operand_type = analyze_expression(node->body.unary_operation.operand);
+      if (operand_type != TOKEN_INT_TYPE) {
+        printf("Semantic error: Unary operator ! requires an integer operand\n");
+        exit(1);
+      }
+      return TOKEN_INT_TYPE;
+    }
+    default:
+      printf("Semantic error: Unsupported node type in expression analysis\n");
+      exit(1);
+  }
 }
 
-*/
+void analyze_node(Node *node) {
+  switch (node->type) {
+    case NODE_PROGRAM:
+      enter_state();
+      for (int i = 0; i < node->body.program.statement_count; i++) {
+        analyze_node(node->body.program.statements[i]);
+      }
+      exit_state();
+      break;
+    case NODE_BLOCK:
+      enter_state();
+      for (int i = 0; i < node->body.block.statement_count; i++) {
+        analyze_node(node->body.block.statements[i]);
+      }
+      exit_state();
+      break;
+    case NODE_VAR_DECLARATION: {
+      TokenType variable_type = node->body.var_declaration.variable_type;
+      char *variable_name = node->body.var_declaration.variable_name;
+      TokenType expr_type = analyze_expression(node->body.var_declaration.variable_value);
+
+      insert_variable(variable_name, variable_type);
+
+      if (expr_type != variable_type) {
+        printf("Semantic error: Type mismatch in assignment to %s\n", variable_name);
+        exit(1);
+      }
+      break;
+    }
+    case NODE_IF_STATEMENT: { 
+      TokenType cond_type = analyze_expression(node->body.if_statement.condition);
+
+      if (cond_type != TOKEN_INT_TYPE) {
+        printf("Semantic error: if condition must be integer\n");
+        exit(1);
+      }
+      analyze_node(node->body.if_statement.then_branch);
+      if (node->body.if_statement.else_branch) {
+        analyze_node(node->body.if_statement.else_branch);
+      }
+      break;
+    }
+    case NODE_PRINT:
+      analyze_expression(node->body.print.print_value);
+      break;
+    default:
+      printf("Semantic error: Unsupported node type in semantic analysis\n");
+      exit(1);
+  }
+}
+
+void semantic_analysis(Node *root) {
+    analyze_node(root);
+}
+
+
+/*
+  TO DO: 
+  Lige så compare den bare: left_type == right_type.
+
+  Vi skal tjekke operator type og så tjekke at det er de rigtige typer for den operator. F.eks.:
+
+  1. Arithmetic (+, -, *, /)
+     Kun integers tilladt
+     Return type: int
+
+  2. Comparison (<, >, <=, >=)
+     Kun integers tilladt
+     Return type: int (bruger int, da vi ikke har boolean)
+
+  3. Equality (==, !=)
+     Egens typer (int == int, string == string)
+     Return type: int
+
+  4. Logical (&&, ||)
+     Kun integers tilladt
+     Return type: int
+*/ 
+
+
+
+
