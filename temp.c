34ddb2ebe8b2629dc58816f52a23c915f6e6fb5f
Williamjacobsen <villi05.v.j@gmail.com>
2026-04-17 11:17:28 +0200

    fixed synced is_shared counter for thread-safety

--- temp.c
+++ temp.c
@@ -2,6 +2,8 @@
                    #include <stdio.h> 
                    #include <pthread.h> 
                    #include <unistd.h>
+pthread_mutex_t lock_counter;
+pthread_mutex_init(&lock_counter, NULL);
 void* a(void* arg) {
 pthread_mutex_lock(&lock_counter);
 counter=(counter+1);pthread_mutex_unlock(&lock_counter);
